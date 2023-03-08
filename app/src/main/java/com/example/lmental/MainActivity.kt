ca1dd9f783e61c203a9ee86e3d99b26986e6e7c3
Emilano Sánchez <62582536+emisc0607@users.noreply.github.com>
2023-03-08 17:16:55 -0600

    View Binding

--- app/src/main/java/com/example/lmental/MainActivity.kt
+++ app/src/main/java/com/example/lmental/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.lmental
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.lmental.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
